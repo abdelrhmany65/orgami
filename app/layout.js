077f6286e802d019395e1591449028970acb6bb8
abdelrhmany65 <abdelrhmany65@gmail.com>
2025-02-23 10:21:53 +0200

    meta tage

--- app/layout.js
+++ app/layout.js
@@ -6,11 +6,27 @@ import "./globals.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
-
 export const metadata = {
-  title: "",
-  description: "",
-
+  title: "OrigamiTechs | حلول تقنية مبتكرة لتطوير الأعمال",
+  description: "نحن شركة متخصصة في تقديم حلول برمجية متكاملة مع فريق من الخبراء. نقدم خدمات تصميم UI/UX، تطوير تطبيقات الجوال، تحليل البيانات، تطوير الويب، وتطوير الأنظمة باستخدام أحدث التقنيات لضمان نجاح أعمالك.",
+  keywords: "OrigamiTechs, حلول تقنية, تطوير البرمجيات, تصميم UI/UX, تطبيقات الجوال, تحليل البيانات, تطوير الويب, تطوير الأنظمة, التكنولوجيا, الأمان, المدينة المنورة, المملكة العربية السعودية",
+  authors: [{ name: "OrigamiTechs" }],
+  openGraph: {
+    title: "OrigamiTechs | حلول تقنية مبتكرة لتطوير الأعمال",
+    description: "اكتشف حلولنا التقنية المتكاملة لتطوير أعمالك باستخدام أحدث التقنيات مع فريق محترف من الخبراء.",
+    url: "https://origmaitechs.com/",
+    type: "website",
+    images: [
+      {
+        // url: "https://origmaitechs.com/path-to-your-image.jpg", // استبدل بعنوان الصورة المناسب
+        // alt: "OrigamiTechs",
+      },
+    ],
+  },
+  // يمكنك إضافة canonical إذا رغبت
+  alternates: {
+    canonical: "https://origmaitechs.com/"
+  }
 };
 
 export default function RootLayout({ children }) {
@@ -20,10 +36,8 @@ export default function RootLayout({ children }) {
       <body>
         <I18nProvider>
           <Header/>
-            
-              {children}
-            
-            <Footer />
+          {children}
+          <Footer />
         </I18nProvider>
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
